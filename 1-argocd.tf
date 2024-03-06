4e25ab73eb039b3fdae550c4daec6454046e115c
Krzysytof Kulaj <krzysztof.kulaj@gmail.com>
2024-03-06 16:12:55 +0100

    initial commit

---
+++ 1-argocd.tf
@@ -0,0 +1,27 @@
+# helm install argocd -n argocd --create-namespace argo/argo-cd --version 3.35.4 -f terraform/values/argocd.yaml
+resource "helm_release" "argocd" {
+  name = "argocd"
+
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "3.35.4"
+
+  values = [file("values/argocd.yaml")]
+}
+
+
+resource "null_resource" "password" {
+  provisioner "local-exec" {
+    command     = "kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath={.data.password} | base64 -d > argocd-login.txt"
+  }
+}
+
+resource "null_resource" "del-argo-pass" {
+  depends_on = [null_resource.password]
+  provisioner "local-exec" {
+    command = "kubectl -n argocd delete secret argocd-initial-admin-secret"
+  }
+}
+
